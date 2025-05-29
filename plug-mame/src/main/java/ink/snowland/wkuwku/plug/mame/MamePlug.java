e76a606320cbeef51411f3b70ca19328a8a5d37d
deliu <de.liu@touchair.cn>
2025-05-29 17:20:15 +0800

    Add: plug icon.

--- plug-mame/src/main/java/ink/snowland/wkuwku/plug/mame/MamePlug.java
+++ plug-mame/src/main/java/ink/snowland/wkuwku/plug/mame/MamePlug.java
@@ -2,12 +2,6 @@ package ink.snowland.wkuwku.plug.mame;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
-
-import androidx.annotation.Nullable;
-import androidx.core.content.res.ResourcesCompat;
 
 import ink.snowland.wkuwku.plug.Plug;
 
@@ -21,17 +15,5 @@ public class MamePlug extends Plug {
     @Override
     public void uninstall() {
         Mame.unregisterEmulator();
-    }
-
-    @Nullable
-    @Override
-    public Bitmap getIcon() {
-        Drawable drawable = ResourcesCompat.getDrawable(resources, R.drawable.mame_logo, null);
-        if (drawable == null) return null;
-        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-        return bitmap;
     }
 }
