b9498318eeda59f4cae727e2b1159170b290fc71
deliu <de.liu@touchair.cn>
2025-05-29 13:17:15 +0800

    Add: impl getIcon method.

--- plug-mame/src/main/java/ink/snowland/wkuwku/plug/mame/MamePlug.java
+++ plug-mame/src/main/java/ink/snowland/wkuwku/plug/mame/MamePlug.java
@@ -2,18 +2,36 @@ package ink.snowland.wkuwku.plug.mame;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+
+import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 
 import ink.snowland.wkuwku.plug.Plug;
 
 public class MamePlug extends Plug {
-    private Resources mResources;
     @Override
     public void install(Context context, Resources resources) {
+        super.install(context, resources);
         Mame.registerAsEmulator(resources);
     }
 
     @Override
     public void uninstall() {
         Mame.unregisterEmulator();
+    }
+
+    @Nullable
+    @Override
+    public Bitmap getIcon() {
+        Drawable drawable = ResourcesCompat.getDrawable(resources, R.drawable.mame_logo, null);
+        if (drawable == null) return null;
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
     }
 }
