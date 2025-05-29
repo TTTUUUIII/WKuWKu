e80c13dd9a7261ab34e1bba554811c04b41bd1aa
deliu <de.liu@touchair.cn>
2025-05-29 08:54:32 +0800

    Update: PlugManifest added some new fields.

--- plug/src/main/java/ink/snowland/wkuwku/plug/PlugManifest.java
+++ plug/src/main/java/ink/snowland/wkuwku/plug/PlugManifest.java
@@ -4,15 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class PlugManifest {
-    public PlugManifest(@NonNull String packageName, @NonNull String mainClass, @Nullable String author) {
+
+    public PlugManifest(@NonNull String name, @NonNull String packageName, @NonNull String mainClass, @Nullable String author, @Nullable String summary) {
+        this.name = name;
         this.packageName = packageName;
         this.mainClass = mainClass;
         this.author = author;
+        this.summary = summary;
     }
 
+    public final String name;
     public final String packageName;
     public final String mainClass;
     public final String author;
+    public final String summary;
     protected String installPath;
     protected String dexFileName;
 }
