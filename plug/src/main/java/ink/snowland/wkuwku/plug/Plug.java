9487450eb82334c329015d9071773d00ebd5379a
deliu <de.liu@touchair.cn>
2025-05-29 11:26:19 +0800

    Add: add some methods for plug.

--- plug/src/main/java/ink/snowland/wkuwku/plug/Plug.java
+++ plug/src/main/java/ink/snowland/wkuwku/plug/Plug.java
@@ -1,10 +1,20 @@
 package ink.snowland.wkuwku.plug;
 
-import android.app.Application;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 
-public interface Plug {
-    void install(Context context, Resources resources);
-    void uninstall();
+import androidx.annotation.NonNull;
+
+public abstract class Plug {
+    private PlugManifest manifest;
+    protected abstract void install(Context context, Resources resources);
+    protected abstract void uninstall();
+    public abstract Bitmap getIcon();
+    public PlugManifest getManifest() {
+        return manifest;
+    }
+    public void setManifest(@NonNull PlugManifest manifest) {
+        this.manifest = manifest;
+    }
 }
