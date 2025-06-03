20a792a3c5f34250b3316108f5eec196d40816a0
deliu <de.liu@touchair.cn>
2025-06-03 16:13:55 +0800

    Add: plug-genesisplusgx module.

---
+++ plug-genesisplusgx/src/main/java/ink/snowland/wkuwku/plug/genesisplusgx/Main.java
@@ -0,0 +1,20 @@
+package ink.snowland.wkuwku.plug.genesisplusgx;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import ink.snowland.wkuwku.plug.Plug;
+
+public class Main extends Plug {
+    @Override
+    protected void install(Context context, Resources resources) {
+        super.install(context, resources);
+        GenesisPlusGX.registerAsEmulator(resources);
+    }
+
+    @Override
+    protected void uninstall() {
+        super.uninstall();
+        GenesisPlusGX.unregisterEmulator();
+    }
+}
