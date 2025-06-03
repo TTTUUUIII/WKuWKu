859d969a4640cc3fd86deb552ac58d998665e7c0
deliu <de.liu@touchair.cn>
2025-06-03 17:00:48 +0800

    New plug: plug-mesen

---
+++ plug-mesen/src/main/java/ink/snowland/wkuwku/plug/mesen/Main.java
@@ -0,0 +1,23 @@
+package ink.snowland.wkuwku.plug.mesen;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import ink.snowland.wkuwku.plug.Plug;
+
+public class Main extends Plug {
+
+    @Override
+    protected void install(Context context, Resources resources) {
+        super.install(context, resources);
+        Mesen.registerAsEmulator(resources);
+        MesenS.registerAsEmulator(resources);
+    }
+
+    @Override
+    protected void uninstall() {
+        super.uninstall();
+        Mesen.unregisterEmulator();
+        MesenS.unregisterEmulator();
+    }
+}
---
+++ plug-mesen/src/main/java/ink/snowland/wkuwku/plug/mesen/Mesen.java
@@ -0,0 +1,108 @@
+package ink.snowland.wkuwku.plug.mesen;
+
+import android.content.res.Resources;
+
+import androidx.annotation.NonNull;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+import ink.snowland.wkuwku.EmulatorManager;
+import ink.snowland.wkuwku.common.EmSystemAvInfo;
+import ink.snowland.wkuwku.common.EmSystemInfo;
+import ink.snowland.wkuwku.interfaces.Emulator;
+
+public class Mesen extends Emulator {
+
+
+    public Mesen(@NonNull Resources res, int configResId) throws XmlPullParserException, IOException {
+        super(res, configResId);
+    }
+
+    public static void registerAsEmulator(@NonNull Resources resources) {
+        if (SHARED_INSTANCE == null) {
+            try {
+                SHARED_INSTANCE = new Mesen(resources, R.xml.mesen_config);
+            } catch (XmlPullParserException | IOException e) {
+                e.printStackTrace(System.err);
+            }
+        }
+        if (SHARED_INSTANCE != null) {
+            EmulatorManager.registerEmulator(SHARED_INSTANCE);
+        }
+    }
+
+    public static void unregisterEmulator() {
+        if (SHARED_INSTANCE != null)
+            EmulatorManager.unregisterEmulator(SHARED_INSTANCE);
+    }
+
+    private static Mesen SHARED_INSTANCE;
+
+    static {
+        System.loadLibrary("mesen-bridge");
+    }
+
+    protected native void nativePowerOn();
+    protected native void nativePowerOff();
+    protected native void nativeReset();
+    protected native boolean nativeLoad(@NonNull String path);
+    protected native void nativeRun();
+    protected native EmSystemAvInfo nativeGetSystemAvInfo();
+    protected native EmSystemInfo nativeGetSystemInfo();
+    protected native boolean nativeSaveMemoryRam(@NonNull String path);
+    protected native boolean nativeLoadMemoryRam(@NonNull String path);
+    protected native boolean nativeSaveState(@NonNull String path);
+    protected native boolean nativeLoadState(@NonNull String path);
+    @Override
+    public String getTag() {
+        return "mesen";
+    }
+
+    @Override
+    public EmSystemInfo getSystemInfo() {
+        return nativeGetSystemInfo();
+    }
+
+    @Override
+    protected EmSystemAvInfo getSystemAvInfo() {
+        return nativeGetSystemAvInfo();
+    }
+
+    @Override
+    public void onPowerOn() {
+        nativePowerOn();
+    }
+
+    @Override
+    public boolean onLoadGame(@NonNull String fullPath) {
+        return nativeLoad(fullPath);
+    }
+
+    @Override
+    public void onNext() {
+        nativeRun();
+    }
+
+    @Override
+    public void onReset() {
+        nativeReset();
+    }
+
+    @Override
+    public void onLoadState(@NonNull String fullPath) {
+        nativeLoadState(fullPath);
+    }
+
+    @Override
+    public boolean onSaveState(@NonNull String savePath) {
+        return nativeSaveState(savePath);
+    }
+
+    @Override
+    public void onPowerOff() {
+        nativePowerOff();
+    }
+}
+
