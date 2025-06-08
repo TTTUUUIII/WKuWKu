ff4f059a174b6a0cdf99b416676dd91aa915c66d
wn123o <wn123o@outlook.com>
2025-06-08 12:05:31 +0800

    Add: plug-yabause modules.

---
+++ plug-yabause/src/main/java/ink/snowland/wkuwku/plug/yabause/Main.java
@@ -0,0 +1,21 @@
+package ink.snowland.wkuwku.plug.yabause;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import ink.snowland.wkuwku.plug.Plug;
+
+public class Main extends Plug {
+
+    @Override
+    protected void install(Context context, Resources resources) {
+        super.install(context, resources);
+        Yabause.registerAsEmulator(resources);
+    }
+
+    @Override
+    protected void uninstall() {
+        super.uninstall();
+        Yabause.unregisterEmulator();
+    }
+}
