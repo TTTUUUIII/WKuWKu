86d9a2e5654c5ac13f2b784a256b29fd8178edd8
wn123o <wn123o@outlook.com>
2025-07-23 22:27:02 +0800

    Update: log format.

--- emulator/src/main/cpp/include/Log.h
+++ emulator/src/main/cpp/include/Log.h
@@ -10,9 +10,9 @@
 #define LOG_TAG "EmulatorV2(Native)"
 #endif
 
-#define LOGD(_tag, _fmt, ...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "[" _tag "] " _fmt, ##__VA_ARGS__)
-#define LOGI(_tag, _fmt, ...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "[" _tag "] " _fmt, ##__VA_ARGS__)
-#define LOGW(_tag, _fmt, ...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, "[" _tag "] " _fmt, ##__VA_ARGS__)
-#define LOGE(_tag, _fmt, ...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "[" _tag "] " _fmt, ##__VA_ARGS__)
+#define LOGD(_tag, _fmt, ...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "[%s] " _fmt, _tag, ##__VA_ARGS__)
+#define LOGI(_tag, _fmt, ...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "[%s] " _fmt, _tag, ##__VA_ARGS__)
+#define LOGW(_tag, _fmt, ...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, "[%s] " _fmt, _tag, ##__VA_ARGS__)
+#define LOGE(_tag, _fmt, ...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "[%s] " _fmt, _tag, ##__VA_ARGS__)
 
 #endif //WKUWKU_LOG_H
--- emulator/src/main/java/ink/snowland/wkuwku/util/Logger.java
+++ emulator/src/main/java/ink/snowland/wkuwku/util/Logger.java
@@ -5,28 +5,35 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 public class Logger {
-    private static final String template = "[%s] %s";
+    private String format = "[%s] %s";
     private final String group;
     private final String file;
 
     public Logger(String group, String file) {
         this.group = group;
         this.file = file;
+        if (file.isEmpty()) {
+            format = "%s%s";
+        }
+    }
+
+    public Logger(String group) {
+        this(group, "");
     }
 
     public void d(@NonNull String fmt, Object...args) {
-        Log.d(group, String.format(template, file, String.format(fmt, args)));
+        Log.d(group, String.format(format, file, String.format(fmt, args)));
     }
 
     public void e(@NonNull String fmt, Object...args) {
-        Log.e(group, String.format(template, file, String.format(fmt, args)));
+        Log.e(group, String.format(format, file, String.format(fmt, args)));
     }
 
     public void i(@NonNull String fmt, Object...args) {
-        Log.i(group, String.format(template, file, String.format(fmt, args)));
+        Log.i(group, String.format(format, file, String.format(fmt, args)));
     }
 
     public void w(@NonNull String fmt, Object...args) {
-        Log.w(group, String.format(template, file, String.format(fmt, args)));
+        Log.w(group, String.format(format, file, String.format(fmt, args)));
     }
 }
