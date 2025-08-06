ce5ef7d1860088366d9b574a4bb5f00e5c92101f
deliu <de.liu@touchair.cn>
2025-08-06 13:09:02 +0800

    Fix: add crashed when exit game (BSNES core).

--- app/src/main/res/raw/default_frgment_shader.glsl
+++
@@ -1,9 +0,0 @@
-#version 300 es
-precision mediump float;
-in vec2 TexCoord;
-uniform sampler2D texture1;
-out vec4 FragColor;
-
-void main() {
-    FragColor = texture(texture1, vec2(TexCoord.x, 1.0 - TexCoord.y));
-}
