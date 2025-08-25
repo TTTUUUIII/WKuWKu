c1991c3751746dff6c74c25acca042d5bf8d413c
deliu <de.liu@touchair.cn>
2025-08-25 09:58:53 +0800

    Del: macro (not easy to use).

--- app/src/main/java/ink/snowland/wkuwku/db/AppDatabase.java
+++ app/src/main/java/ink/snowland/wkuwku/db/AppDatabase.java
@@ -1,5 +1,7 @@
 package ink.snowland.wkuwku.db;
 
+import static ink.snowland.wkuwku.db.Migrations.*;
+
 import android.content.Context;
 
 import androidx.room.Database;
@@ -7,19 +9,18 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import ink.snowland.wkuwku.db.dao.GameDao;
-import ink.snowland.wkuwku.db.dao.MacroScriptDao;
 import ink.snowland.wkuwku.db.dao.PlugManifestExtDao;
 import ink.snowland.wkuwku.db.entity.Game;
-import ink.snowland.wkuwku.db.entity.MacroScript;
 import ink.snowland.wkuwku.db.entity.PlugManifestExt;
 
-@Database(entities = {Game.class, MacroScript.class, PlugManifestExt.class}, version = 1)
+@Database(entities = {Game.class, PlugManifestExt.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract GameDao gameInfoDao();
-    public abstract MacroScriptDao macroScriptDao();
     public abstract PlugManifestExtDao plugManifestExtDao();
     public static AppDatabase db;
     public static void initialize(Context applicationContext) {
-        db = Room.databaseBuilder(applicationContext, AppDatabase.class, "db-wkuwku").build();
+        db = Room.databaseBuilder(applicationContext, AppDatabase.class, "db-wkuwku")
+                .addMigrations(MIGRATION_1_2)
+                .build();
     }
 }
