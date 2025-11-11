089b52671eacee44a2982009ab3857aa27c47e27
de.liu <de.liu@touchair.cn>
2025-11-11 09:05:06 +0800

    Add: plug-mame.

---
+++ plug-mame/src/main/java/org/wkuwku/plug/mame/Main.java
@@ -0,0 +1,19 @@
+package org.wkuwku.plug.mame;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import ink.snowland.wkuwku.plug.Plug;
+
+public class Main extends Plug {
+    @Override
+    public void install(Context context, Resources resources) {
+        super.install(context, resources);
+        Mame.registerAsEmulator(resources);
+    }
+
+    @Override
+    public void uninstall() {
+        Mame.unregisterEmulator();
+    }
+}
