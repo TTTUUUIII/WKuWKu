f6fed55cd5392f22bf2131c67ae036a02d844208
de.liu <de.liu@touchair.cn>
2026-02-03 16:05:22 +0800

    Opt: layout.

---
+++ app/src/main/java/ink/snowland/wkuwku/widget/SimpleGridItemDecoration.java
@@ -0,0 +1,31 @@
+package ink.snowland.wkuwku.widget;
+
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SimpleGridItemDecoration extends RecyclerView.ItemDecoration {
+    private int mItemWidthInPx;
+    private int mSpanCount;
+    private int mTotalWidth;
+
+    public SimpleGridItemDecoration(int itemWidthInPx, int spanCount, int totalWidth) {
+        mItemWidthInPx = itemWidthInPx;
+        mSpanCount = spanCount;
+        mTotalWidth = totalWidth;
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        int spacing = mTotalWidth - mItemWidthInPx * mSpanCount;
+        if (spacing > 0) {
+            int itemHorizontalSpacing = spacing / (mSpanCount * 2);
+            outRect.set(itemHorizontalSpacing, 40, itemHorizontalSpacing, 40);
+            return;
+        }
+        super.getItemOffsets(outRect, view, parent, state);
+    }
+}
