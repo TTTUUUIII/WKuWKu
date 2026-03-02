0347cea47430eded66b018f72b31ae1c027d3d1d
de.liu <de.liu@touchair.cn>
2026-03-02 16:04:42 +0800

    Add: plug-swanstation.

---
+++ plug-swanstation/src/main/java/org/wkuwku/plug/swanstation/Main.java
@@ -0,0 +1,20 @@
+package org.wkuwku.plug.swanstation;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import ink.snowland.wkuwku.plug.Plug;
+
+public class Main extends Plug {
+    @Override
+    protected void install(Context context, Resources resources) {
+        super.install(context, resources);
+        Swanstation.registerAsEmulator(resources);
+    }
+
+    @Override
+    protected void uninstall() {
+        super.uninstall();
+        Swanstation.unregisterEmulator();
+    }
+}
