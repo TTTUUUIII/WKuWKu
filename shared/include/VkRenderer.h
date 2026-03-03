7bb82c957fb621e20d1aac92cb9854935b6fbc12
wn123o <wn123o@outlook.com>
2026-03-03 22:28:25 +0800

    Opt: vulkan renderer.

--- shared/include/VkRenderer.h
+++ shared/include/VkRenderer.h
@@ -10,33 +10,56 @@
 #include <set>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <utility>
 #include "Renderer.h"
 #include "VkContext.h"
 #include "Utils.h"
 
-struct vk_buffer_t {
+
+class vk_buffer_t {
+private:
     VkDevice dev;
-    VkBuffer buffer;
-    VkDeviceMemory memory;
-    void *data{};
+    VkBuffer buf{};
+    VkDeviceMemory mem{};
+    void *data_ptr = nullptr;
+public:
+    vk_buffer_t(VkDevice, VkPhysicalDevice, VkBufferUsageFlags, VkMemoryPropertyFlags,
+                         VkDeviceSize);
+    ~vk_buffer_t();
+    void* data();
+    [[nodiscard]] VkBuffer buffer() const;
+    static uint32_t find_memory_type_index(VkPhysicalDevice, uint32_t /*type_filter*/, VkMemoryPropertyFlags /*properties*/);
+};
 
-    explicit vk_buffer_t(VkDevice _dev, VkBuffer _buf, VkDeviceMemory _mem, VkDeviceSize _size) : dev(_dev),
-                                                                              buffer(_buf),
-                                                                              memory(_mem){
-        vkMapMemory(dev, memory, 0, _size, 0, &data);
-    }
+class vk_swap_chain_t {
+private:
+    std::mutex mtx;
+    std::vector<std::shared_ptr<vk_buffer_t>> buffers;
+    std::queue<int> free_idxs;
+    std::queue<int> full_idxs;
+    int cur_read_idx = -1;
+public:
+    explicit vk_swap_chain_t(VkDevice,
+                             VkPhysicalDevice,
+                             VkBufferUsageFlags,
+                             VkMemoryPropertyFlags,
+                             VkDeviceSize,
+                             uint32_t);
 
-    ~vk_buffer_t() {
-        vkUnmapMemory(dev, memory);
-        vkFreeMemory(dev, memory, nullptr);
-        vkDestroyBuffer(dev, buffer, nullptr);
-        data = nullptr;
-    }
+    int acquire_write_idx();
+
+    void submit(int idx);
+
+    std::shared_ptr<vk_buffer_t> get(int idx);
+
+    int acquire_read_idx();
+
+    virtual ~vk_swap_chain_t();
 };
 
 class VkRenderer : public Renderer {
 private:
-    const int MAX_FRAMES_IN_FLIGHT = 2;
+    const int MAX_FRAMES_IN_FLIGHT = 3;
     std::unique_ptr<VkContext> context;
     ANativeWindow *window;
     std::thread vk_thread;
@@ -60,8 +83,7 @@ private:
     VkImageView tex_view{};
     VkDeviceMemory tex_mem{};
     VkSampler tex_sampler{};
-    std::shared_ptr<vk_buffer_t> tex_staging_buffer;
-    std::atomic<bool> tex_staging_buffer_updated = false;
+    std::unique_ptr<vk_swap_chain_t> tex_swap_chain;
     VkSwapchainKHR swap_chain{};
     queue_info_t graphics_queue_info{};
     queue_info_t present_queue_info{};
@@ -97,6 +119,7 @@ private:
     void create_command_buffers();
 
     void create_texture();
+
     void update_texture(VkCommandBuffer);
 
     void create_texture_sampler();
@@ -105,15 +128,12 @@ private:
 
     void create_sync_objects();
 
-    std::shared_ptr<vk_buffer_t> create_buffer(VkBufferUsageFlags, VkMemoryPropertyFlags, VkDeviceSize);
-
     void recreate_swap_chain();
 
     void clean_swap_chain();
 
-    void copy_buffer(const vk_buffer_t& /*src*/, uint32_t /*src_offset*/, vk_buffer_t& /*dst*/, uint32_t /*dst_offset*/, VkDeviceSize /*len*/);
-
-    uint32_t find_mem_type(uint32_t filter, VkMemoryPropertyFlags properties);
+//    void copy_buffer(vk_buffer_t& /*src*/, uint32_t /*src_offset*/, vk_buffer_t& /*dst*/,
+//                     uint32_t /*dst_offset*/, VkDeviceSize /*len*/);
 
     VkShaderModule create_shader_mode(const u_int8_t *bytes, size_t size_in_bytes);
 
@@ -125,6 +145,7 @@ private:
     void end_single_time_commands(VkCommandBuffer);
 
     void update_uniform_buffer();
+
     void update_texcoords();
 
     void record_command_buffer(VkCommandBuffer /*buffer*/, u_int32_t /*image index*/);
@@ -138,12 +159,18 @@ private:
     void choose_device_extensions(bool /*use_swappy*/);
 
     void enable_swappy(JNIEnv *env, jobject activity);
+
     static void
-    upload_image(const VkCommandBuffer&, VkBuffer /*buffer*/, VkImage /*image*/, uint32_t /*width*/,
-           uint32_t /*height*/);
-    static void download_image(const VkCommandBuffer&, const VkImage&, uint32_t, uint32_t, VkBuffer& dst);
-    static void transition_image_layout(VkCommandBuffer, VkImage, VkFormat, VkImageLayout /*old_layout*/,
-                                  VkImageLayout /*new_layout*/);
+    upload_image(VkCommandBuffer, VkBuffer /*buffer*/, VkImage /*image*/,
+                 uint32_t /*width*/,
+                 uint32_t /*height*/);
+
+    static void
+    download_image(VkCommandBuffer, VkImage, uint32_t, uint32_t, VkBuffer);
+
+    static void
+    transition_image_layout(VkCommandBuffer, VkImage, VkFormat, VkImageLayout /*old_layout*/,
+                            VkImageLayout /*new_layout*/);
 
 public:
     explicit VkRenderer(JNIEnv */*env*/, jobject /*activity*/, jobject /*surface*/);
